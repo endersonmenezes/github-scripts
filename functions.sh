bd80a3c762466121fa92c43cbf8f4f745eecdd6e
Enderson Menezes <endersonster@gmail.com>
2024-08-15 15:07:35 -0400

    refactor: Refactor audit_file function in functions.sh
    Refactor the audit_file function in functions.sh to improve readability and efficiency.
    - Use double quotes for file paths to handle spaces and special characters correctly.
    - Store the output of sha256sum in a variable to avoid unnecessary command execution.
    - Update the echo statement to include the value of the SHA256 hash.

--- functions.sh
+++ functions.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
 # Functions that can be used in other scripts
+# Author: Enderson Menezes
+# E-mail: mail@enderson.dev
 
 ## SHA Function
 # That function will create a SHA256 hash of the file that is being executed
 
 audit_file(){
-    SHA256=$(sha256sum $0 | cut -d' ' -f1)
-    echo "Executing a file: $0, with SHA256: $SHA256"
+    FILE_SHA256=$(sha256sum "$0")
+    SHA256=$(cut -d' ' -f1 <<< "${FILE_SHA256}")
+    echo "Executing a file: $0, with SHA256: ${SHA256}"
 }
 
 
@@ -14,12 +19,12 @@ audit_file(){
 
 read_config_file(){
     ## Read a CSV file (owner-repo,team)
-    SCRIPT_NAME_WITHOUT_EXTENSION=$(basename $0 | cut -d'.' -f1)
-    FILE="$SCRIPT_NAME_WITHOUT_EXTENSION.csv"
+    SCRIPT_NAME_WITHOUT_EXTENSION=$(basename "$0" | cut -d'.' -f1)
+    FILE="${SCRIPT_NAME_WITHOUT_EXTENSION}.csv"
 
     # Verify if the file exists
-    if [ ! -f $FILE ]; then
-        echo "The file $FILE does not exist."
+    if [[ ! -f "${FILE}" ]]; then
+        echo "The file ${FILE} does not exist."
         exit 1
     fi
 }
