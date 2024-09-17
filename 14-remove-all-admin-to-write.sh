80e53e992a7ed3e2cb5bb3ae2b4ddba469f3eb46
Enderson Menezes <endersonster@gmail.com>
2024-09-17 11:43:11 -0300

    feat: Add script to downgrade team permissions from admin to write

---
+++ 14-remove-all-admin-to-write.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+##
+# Author: Enderson Menezes
+# Created: 2024-08-12
+# Description: This script downgrade all team permission on repositories from admin to write.
+# Usage: bash 14-remove-all-admin-to-write.sh <org> <team>
+##
+
+# Read Common Functions
+source functions.sh
+
+# Verify GH is installed
+is_gh_installed
+
+# Create a SHA256 of the file for audit (Define SHA256 varible)
+audit_file
+
+# Args
+PARALLEL_JOBS=2
+ORGANIZATION=$1
+TEAM=$2
+REPO_FILES="repos.json"
+
+# Function to downgrade team permission
+function update_permission(){
+    OWNER=$1
+    TEAM=$2
+    REPO=$3
+    PERMISSION=$4
+    API_URL="/orgs/$OWNER/teams/$TEAM/repos/$OWNER/$REPO"
+    echo "Downgrading ${TEAM} to ${TARGET_ROLE} on ${OWNER}/${REPO} on ${API_URL}" 
+    gh api \
+        --method PUT \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        $API_URL \
+        -f "permission=${PERMISSION}" > /dev/null
+}
+
+# List team repositories
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  --paginate \
+  /orgs/$ORGANIZATION/teams/$TEAM/repos > $REPO_FILES
+
+# For item in REPO_FILES
+for row in $(jq -r '.[] | @base64' $REPO_FILES); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+    OWNER=$(_jq '.owner.login')
+    REPO=$(_jq '.name')
+    ACTUAL_ROLE=$(_jq '.role_name')
+    TARGET_ROLE="push"
+    # if role name is admin, downgrade to write
+    if [[ "${ACTUAL_ROLE}" == "admin" ]]; then
+        update_permission $OWNER $TEAM $REPO "$TARGET_ROLE"
+    fi
+done
